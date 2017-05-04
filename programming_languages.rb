79c432a4a64fa047de37283b0481d4235f9d3cfc
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-04 21:51:30 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |type, hash|
+    hash.each do |name, category|
+      new_hash[name] ||= category
+      new_hash[name][:style] ||= []
+      new_hash[name][:style] << type
+    end
+  end
+  new_hash
 end
